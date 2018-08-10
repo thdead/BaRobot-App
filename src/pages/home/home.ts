41702c5d2118a22d3912a390430e3067c4515fac
unknown <nathoun68@hotmail.com>
2018-08-10 11:07:38 +0200

    ajout des pages et slider

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Slides } from 'ionic-angular/index';
+import { AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
+
 export class HomePage {
+  @ViewChild(Slides) slides: Slides;
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  goToSlide() {
+    this.slides.slideTo(2, 5000);
   }
 
 }
