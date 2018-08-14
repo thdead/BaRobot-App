0b2bd20e13b5023afc576e521df32ee4ea7e00e4
unknown <nathoun68@hotmail.com>
2018-08-14 03:05:36 +0200

    nettoyage du code

--- src/pages/bluetooth/bluetooth.ts
+++ src/pages/bluetooth/bluetooth.ts
@@ -25,16 +25,6 @@ export class BluetoothPage {
   connectedDeviceId: string = "";
   
 
-  devices: Array<any> = [];
-  mostrarSpiner = true;
-  mensaje: string = "";
-  conexion: ISubscription;
-  conexionMensajes: ISubscription;
-  reader: Observable<any>;
-  rawListener;
-
-
-
   constructor(private toastCtrl: ToastController, private alertCtrl: AlertController, private bluetoothSerial: BluetoothSerial, private loadingCtrl: LoadingController) { }
 
 
@@ -184,8 +174,8 @@ export class BluetoothPage {
   isConnectedTo(){
     this.bluetoothSerial.isConnected().then(
       succes => {
-        this.createToast('Device connected, status : ' + succes);
         this.isConnected = true;
+        this.createToast('Device connected, status : ' + succes);
       },
       failure => {
         this.connectedDeviceName = '';
@@ -219,9 +209,7 @@ export class BluetoothPage {
         this.createToast("You can't send message if you are not connected");
         this.toSend = '';
       }
-    );
-    
-    
+    ); 
   }
   
 
@@ -257,194 +245,4 @@ export class BluetoothPage {
     toast.present();
   }
 
-  /**
-   * ------------------------------------------------------------------------------------------------------------------------------
-   * ------------------------------------------------------------------------------------------------------------------------------
-   * ------------------------------------------------------------------------------------------------------------------------------
-   */
-
-  /**
-   * Recherchez les périphériques Bluetooth disponibles, évaluez s'il est possible d'utiliser la fonctionnalité
-   * Bluetooth sur l'appareil.
-   * @return {Promise<any>} Renvoie une liste des périphériques localisés.
-   */
-  buscarBluetooth(): Promise<Object> {
-    return new Promise((resolve, reject) => {
-      this.bluetoothSerial.isEnabled().then(
-        success =>{
-          this.bluetoothSerial.discoverUnpaired().then(success => {
-            if (success.length > 0) {
-              resolve(success);
-            } else {
-              reject('Aucun appareil trouvé');
-            }
-          }).catch((error) => {
-            console.log(`[1] Error: ${JSON.stringify(error)}`);
-            reject('Bluetooth non disponible sur cette plateforme');
-          });
-        }, fail => {
-          console.log(`[2] Error: ${JSON.stringify(fail)}`);
-          reject("Bluetooth n'est pas disponible ou est désactivé");
-        }
-      );
-    });
-  }
-  /**
-   * Recherchez les périphériques bluetooth en faisant glisser l'écran vers le bas.
-   * @param refresher
-   */
-  refreshBluetooth(refresher: Refresher) {
-    console.log(refresher);
-    if (refresher) {
-      this.buscarBluetooth().then((successMessage: Array<Object>) => {
-        this.devices = [];
-        this.devices = successMessage;
-        refresher.complete();
-      }, fail => {
-        this.presentToast(fail);
-        refresher.complete();
-      });
-    }
-  }
-  /**
-   * Vérifiez si vous êtes déjà connecté à un périphérique Bluetooth ou non.
-   * @param seleccion Ce sont les données de l'élément sélectionné dans la liste
-   */
-  revisarConexion(seleccion) {
-    this.bluetoothSerial.isConnected().then(
-      isConnected => {
-        let alert = this.alertCtrl.create({
-          title: 'Reconnexion',
-          message: 'Voulez-vous vous reconnecter à cet appareil?',
-          buttons: [
-            {
-              text: 'Annuler',
-              role: 'cancel',
-              handler: () => {
-                console.log('Cancel clicked');
-              }
-            },
-            {
-              text: 'Accepter',
-              handler: () => {
-                this.desconectar();
-                this.conectar(seleccion.id).then(success => {
-                  this.presentToast(success);
-                }, fail => {
-                  this.presentToast(fail);
-                });
-              }
-            }
-          ]
-        });
-        alert.present();
-      }, notConnected => {
-        let alert = this.alertCtrl.create({
-          title: 'Se connecter',
-          message: "Voulez-vous connecter l'appareil?",
-          buttons: [
-            {
-              text: 'Annuler',
-              role: 'cancel',
-              handler: () => {
-                console.log('Cancel clicked');
-              }
-            },
-            {
-              text: 'Accepter',
-              handler: () => {
-                this.conectar(seleccion.id).then(success => {
-                  this.presentToast(success);
-                }, fail => {
-                  this.presentToast(fail);
-                });
-              }
-            }
-          ]
-        });
-        alert.present();
-    });
-  }
-  /**
-   * Un périphérique Bluetooth est conçu pour son identifiant.
-   * @param id C'est l'identifiant du périphérique auquel vous voulez vous connecter
-   * @return {Promise<any>} Renvoyer un message pour indiquer s'il a été connecté avec succès ou non.
-   */
-  conectar(id: string): Promise<any> {
-    return new Promise((resolve, reject) => {
-      this.conexion = this.bluetoothSerial.connect(id).subscribe((data: Observable<any>) => {
-        this.enviarMensajes();
-        resolve("Connecté");
-      }, fail => {
-        console.log(`[3] Erreur de connexion: ${JSON.stringify(fail)}`);
-        reject("Impossible de se connecter");
-      });
-    });
-  }
-  /**
-   * Fermez le support pour la connexion avec un périphérique Bluetooth.
-   */
-  desconectar() {
-    if (this.conexionMensajes) {
-      this.conexionMensajes.unsubscribe();
-    }
-    if (this.conexion) {
-      this.conexion.unsubscribe();
-    }
-  }
-  /**
-   * Il permet d'envoyer des messages texte en série lors de la connexion via Bluetooth.
-   */
-  enviarMensajes() {
-    this.conexionMensajes = this.dataInOut(this.mensaje).subscribe(data => {
-      let entrada = data.substr(0, data.length - 1);
-      if (entrada != ">") {
-        if (entrada != "") {
-          console.log(`Entrée: ${entrada}`);
-          this.presentToast(entrada);
-        }
-      } else {
-        this.conexionMensajes.unsubscribe();
-      }
-      this.mensaje = "";
-    });
-  }
-  /**
-   * Définit le socket pour les communications série après la connexion à un périphérique
-   * bluetooth.
-   * @param message C'est le texte que vous souhaitez envoyer.
-   * @returns {Observable<any>} Renvoyer le texte qui arrive via la connexion via
-   * Bluetooth à l'appareil, en cas d'absence de connexion, un message indiquant:
-   * _Vous n'êtes connecté à aucun périphérique Bluetooth_.
-   */
-  public dataInOut(message: string): Observable<any> {
-    return Observable.create(observer => {
-      this.bluetoothSerial.isConnected().then(isConnected => {
-        this.reader = Observable.fromPromise(this.bluetoothSerial.write(message))
-          .flatMap(() => {
-            return this.bluetoothSerial.subscribeRawData()
-          })
-          .flatMap(() => {
-            return this.bluetoothSerial.readUntil('\n');   // <= délimiteur
-          });
-        this.reader.subscribe(data => {
-          observer.next(data);
-        });
-      }, notConected => {
-        observer.next("Vous êtes déconnecté ");
-        observer.complete();
-      });
-    });
-  }
-  /**
-   * Présenter une boîte de message.
-   * @param {string} text Message à montrer
-   */
-  private presentToast(text: string) {
-    let toast = this.toastCtrl.create({
-      message: text,
-      duration: 3000
-    });
-    toast.present();
-  }
 }
